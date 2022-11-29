230f623e8187698e886aa084c071d1446c82627a
Rafael Oliveira <rafaeloliveirafinanca@gmail.com>
2022-11-29 14:09:47 -0300

    Projetinho finalizado

---
+++ main.js
@@ -0,0 +1,13 @@
+const btnMobile = document.querySelector('.cabecalho__menu-hamburguer');
+
+const toogleMenu =  (e) => {
+    const nav = document.querySelector('.menu__lista')
+    nav.classList.toggle('active')
+
+}
+
+
+btnMobile.addEventListener('click', toogleMenu)
+btnMobile.addEventListener('blur', toogleMenu)
+
+
